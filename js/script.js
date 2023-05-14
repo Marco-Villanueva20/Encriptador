be40f057b613ee31dba84c98f4e4cd444f38983c
Marco <marco_x1230@hotmail.com>
2023-05-13 21:28:51 -0500

    Añadiendo validaciones de ingrezo de texto

--- js/script.js
+++ js/script.js
@@ -1,43 +1,50 @@
 
     
-var copiar = document.getElementById("btn-copiar");
-var encriptar = document.getElementById("btn-encriptar");
-var desencriptar = document.getElementById("btn-desencriptar");
+    var copiar = document.getElementById("btn-copiar");
+    var encriptar = document.getElementById("btn-encriptar");
+    var desencriptar = document.getElementById("btn-desencriptar");
 
-encriptar.addEventListener("click", function() {
-    let mensaje = document.getElementById("mensaje");
-    let texto = document.getElementById("texto").value;
-    
-    let tituloMensaje = document.getElementById("titulo-mensaje");
-    let parrafo = document.getElementById("mensaje-ayuda");
-    let munheco = document.getElementById("munheco");
+    encriptar.addEventListener("click", function() {
+        let mensaje = document.getElementById("mensaje");
+        let texto = document.getElementById("texto").value;
+        
+        let tituloMensaje = document.getElementById("titulo-mensaje");
+        let parrafo = document.getElementById("mensaje-ayuda");
+        let munheco = document.getElementById("munheco");
 
-  let textoCifrado = texto
-        .replace(/e/gi, "enter")
-        .replace(/i/gi, "imes")
-        .replace(/a/gi, "ai")
-        .replace(/o/gi, "ober")
-        .replace(/u/gi, "ufat");
+    let textoCifrado = texto
+            .replace(/e/gi, "enter")
+            .replace(/i/gi, "imes")
+            .replace(/a/gi, "ai")
+            .replace(/o/gi, "ober")
+            .replace(/u/gi, "ufat");
 
-  if (texto.length !== 0) {
-    munheco.style.display = "none";
-    parrafo.style.display = "none";
-    tituloMensaje.style.display = "none";
+    if (texto.length <= 0) {
+        munheco.style.display = "flex";
+        mensaje.style.display = "none";
+        copiar.style.display="none";
+        tituloMensaje.style.display = "flex";
+        parrafo.style.display = "flex";
+        swal("Ohh nooo!!!","Debes ingresar algún texto para encriptar","warning");
+    } else if(/a-z/.test(texto)) {
+        munheco.style.display = "none";
+        parrafo.style.display = "none";
+        tituloMensaje.style.display = "none";
 
-    mensaje.style.display = "flex";
-    mensaje.style.position = "absolute";
-    copiar.style.display = "flex";
-    mensaje.value = textoCifrado;
-   
-  } else {
-    munheco.style.display = "flex";
-    mensaje.style.display = "none";
-    copiar.style.display="none";
-    tituloMensaje.style.display = "flex";
-    parrafo.style.display = "flex";
-    swal("Ohh nooo!!!","Debes ingresar algún texto para encriptar","warning");
-  }
-});
+        mensaje.style.display = "flex";
+        mensaje.style.position = "absolute";
+        copiar.style.display = "flex";
+        mensaje.value = textoCifrado;
+    }else{
+        munheco.style.display = "flex";
+        mensaje.style.display = "none";
+        copiar.style.display="none";
+        tituloMensaje.style.display = "flex";
+        parrafo.style.display = "flex";
+        swal("Ohh nooo!!!","Debes ingresar solo letras a-z","warning");
+        texto.value="";
+    }
+    });
 
 desencriptar.addEventListener("click",function(){
     let mensaje = document.getElementById("mensaje");
@@ -54,7 +61,15 @@ desencriptar.addEventListener("click",function(){
     .replace(/ober/gi, "o")
     .replace(/ufat/gi, "u");
 
-  if (texto.length !== 0) {
+  if (texto.length <= 0) {
+    munheco.style.display = "flex";
+    mensaje.style.display = "none";
+    copiar.style.display="none";
+    tituloMensaje.style.display = "flex";
+    parrafo.style.display = "flex";
+    swal("Oh nooo!!!","Debes ingresar algún texto para desencriptar","warning");
+   
+  } else if(/a-z/.test(texto)){
     munheco.style.display = "none";
     parrafo.style.display = "none";
     tituloMensaje.style.display = "none";
@@ -63,14 +78,15 @@ desencriptar.addEventListener("click",function(){
     mensaje.style.position = "absolute";
     copiar.style.display = "flex";
     mensaje.value = textoCifrado;
-   
-  } else {
-    munheco.style.display = "flex";
-    mensaje.style.display = "none";
-    copiar.style.display="none";
-    tituloMensaje.style.display = "flex";
-    parrafo.style.display = "flex";
-    swal("Oh nooo!!!","Debes ingresar algún texto para desencriptar","warning");}
+    }else{
+        munheco.style.display = "flex";
+        mensaje.style.display = "none";
+        copiar.style.display="none";
+        tituloMensaje.style.display = "flex";
+        parrafo.style.display = "flex";
+        swal("Oh nooo!!!","Debes ingresar solo letras, verifica","warning");
+        texto.value="";
+    }
 });
 
 copiar.addEventListener("click", copiarTexto);
@@ -78,15 +94,21 @@ copiar.addEventListener("touchend", copiarTexto);
 
 function copiarTexto() {
     let mensaje = document.getElementById("mensaje").value;
-    //let eti_mensaje = document.getElementById("mensaje");
-    //eti_mensaje.select();si desean seleccionar
-    //document.execCommand(mensaje); obsoleto
+    let texto = document.getElementById("texto").value;
+
     navigator.clipboard.writeText(mensaje);
     swal("Texto copiado",{
       icon: 'success',
       buttons: false,
       timer: 1500
     });
+    munheco.style.display = "flex";
+    mensaje.style.display = "none";
+    tituloMensaje.style.display = "flex";
+    parrafo.style.display = "flex";
+    copiar.style.display="none";
+    texto="";
+    mensaje ="";
   }
 
   function convertir(event) {
